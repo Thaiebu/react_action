e1cae063775b7d92e628028cb612839be7e36ac9
Thaiebu <thaiebu785@gmail.com>
2022-09-13 14:46:33 +0530

    update code

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    {/* <CountApple /> */}
-    {/* <Fruit name="something" />
-    <Fruit name="Apple" /> */}
+    <CountApple />
+    <Fruit name="something" />
+    <Fruit name="Apple" />
   </React.StrictMode>
 );
 
